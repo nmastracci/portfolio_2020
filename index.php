31d30a9b21596070c25c9be487ea55223673c02e
Natalie Mastracci <mastracci.natalie@gmail.com>
2020-03-07 11:24:03 -0500

    redesigns index adding artwork piece

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Natalie Mastracci | Portfolio</title>
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700|Barlow+Condensed&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="prod/css/styles.css">
+  <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+</head>
+
+<body>
+  <nav class="nav"></nav>
+  <header class="header">
+    <div class="left">
+      <h1>Natalie<br>Mastracci</h1>
+      <h3>Develop | Design | Deliver</h3>
+    </div>
+    <div class="right">
+      <div class="triangleBottomRight"></div>
+    </div>
+  </header>
+
+  <section id="artwork-page">
+    <img id="name1" class="backwhite" src="images/n_logo-backwhite.png" alt="n logo">
+    <svg id="rect1" viewBox="0 0 1200 400" xmlns="http://www.w3.org/2000/svg" version="1.1">
+      <rect width="400" height="200" fill="#dee2e6"></rect>
+    </svg>
+    <div id="walking"></div>
+
+    <div id="ashley"></div>
+    <svg id="rect2" viewBox="0 0 1200 400" xmlns="http://www.w3.org/2000/svg" version="1.1">
+      <rect x="700" width="400" height="200" fill="#dee2e6"></rect>
+    </svg>
+    <div id="christine"></div>
+    <img id="name2" class="backwhite" src="images/m_logo-backwhite.png" alt="letter m">
+    <div id="purpleDress"></div>
+    <svg id="rect3" viewBox="0 0 1200 400" xmlns="http://www.w3.org/2000/svg" version="1.1">
+      <rect x="500" width="300" height="700" fill="#dee2e6"></rect>
+    </svg>
+    <div id="profile"></div>
+    <img id="name3" class="backwhite" src="images/n_logo-backwhite.png" alt="n logo">
+    <img id="name4" class="backwhite" src="images/m_logo-backwhite.png" alt="letter m">
+  </section>
+
+  <script type="text/javascript" src="prod/js/scripts.js"></script>
+</body>
+
+</html>
