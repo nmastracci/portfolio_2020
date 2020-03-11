c4e08feb03a49a16f063001796241e7c0a8e37b5
Natalie Mastracci <mastracci.natalie@gmail.com>
2020-03-10 22:57:19 -0400

    adds case study and responsive layout

--- index.php
+++ index.php
@@ -11,39 +11,75 @@
 </head>
 
 <body>
-  <nav class="nav"></nav>
-  <header class="header">
-    <div class="left">
-      <h1>Natalie<br>Mastracci</h1>
-      <h3>Develop | Design | Deliver</h3>
-    </div>
-    <div class="right">
-      <div class="triangleBottomRight"></div>
-    </div>
-  </header>
+  <div id="container">
+    <nav class="nav"></nav>
+    <header class="header">
+      <div class="left">
+        <h1>Natalie<br>Mastracci</h1>
+        <h3>Design | Develop | Deliver</h3>
+      </div>
+      <div class="right">
+        <div class="triangleBottomRight"></div>
+      </div>
+    </header>
 
-  <section id="artwork-page">
-    <img id="name1" class="backwhite" src="images/n_logo-backwhite.png" alt="n logo">
-    <svg id="rect1" viewBox="0 0 1200 400" xmlns="http://www.w3.org/2000/svg" version="1.1">
-      <rect width="400" height="200" fill="#dee2e6"></rect>
-    </svg>
-    <div id="walking"></div>
+    <section id="artwork-page">
+      <img id="name1" class="backwhite" src="images/n_logo-backwhite.png" alt="n logo">
+      <svg id="rect1" viewBox="0 0 1200 400" xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <rect width="400" height="200" fill="#dee2e6"></rect>
+      </svg>
+      <div id="walking"></div>
 
-    <div id="ashley"></div>
-    <svg id="rect2" viewBox="0 0 1200 400" xmlns="http://www.w3.org/2000/svg" version="1.1">
-      <rect x="700" width="400" height="200" fill="#dee2e6"></rect>
-    </svg>
-    <div id="christine"></div>
-    <img id="name2" class="backwhite" src="images/m_logo-backwhite.png" alt="letter m">
-    <div id="purpleDress"></div>
-    <svg id="rect3" viewBox="0 0 1200 400" xmlns="http://www.w3.org/2000/svg" version="1.1">
-      <rect x="500" width="300" height="700" fill="#dee2e6"></rect>
-    </svg>
-    <div id="profile"></div>
-    <img id="name3" class="backwhite" src="images/n_logo-backwhite.png" alt="n logo">
-    <img id="name4" class="backwhite" src="images/m_logo-backwhite.png" alt="letter m">
-  </section>
+      <div id="ashley"></div>
+      <svg id="rect2" viewBox="0 0 1200 400" xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <rect x="700" width="400" height="200" fill="#dee2e6"></rect>
+      </svg>
+      <div id="christine"></div>
+      <img id="name2" class="backwhite" src="images/m_logo-backwhite.png" alt="letter m">
+      <div id="purpleDress"></div>
+      <svg id="rect3" viewBox="0 0 1200 400" xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <rect x="500" width="300" height="700" fill="#dee2e6"></rect>
+      </svg>
+      <div id="profile"></div>
+      <img id="name3" class="backwhite" src="images/n_logo-backwhite.png" alt="n logo">
+      <img id="name4" class="backwhite" src="images/m_logo-backwhite.png" alt="letter m">
+    </section>
 
+    <section id="casestudy">
+      <h3 class="casestudy-subtitle">case study</h3>
+      <h2 class="casestudy-title">Inspect-It</h2>
+      <p class="description">Inspection software designed for the material handling industry with vehicles in mind.</p>
+      <div class="explanation">
+        <h4>The challenge</h4>
+        <div>
+          <p>Make it easier for users to set up their first inspection forms. A high percentage of users were signing up but a low ratio were using the product.
+          </p>
+        </div>
+      </div>
+      <div class="explanation">
+        <h4>Areas of Concern</h4>
+        <div>
+          <p>
+            An older user-base with a lower tolerance for web-based on-boarding and less familiarity with techology presents a smaller window for engagement. Dated branding and site design did not lend itself to convey credibility or imbue trust in the user.
+          </p>
+        </div>
+      </div>
+      <div class="explanation">
+        <h4>Primary Goals</h4>
+        <div>
+          <p>
+            Decrease bounce-rate after sign-up by creating an more user-friendly inspection creation wizard. Create cohesive and consistent branding.
+          </p>
+        </div>
+      </div>
+
+    </section>
+
+    <footer>
+      <i class="fas fa-paper-plane"></i>
+    </footer>
+
+  </div>
   <script type="text/javascript" src="prod/js/scripts.js"></script>
 </body>
 
